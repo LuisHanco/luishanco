fa0a147422f3523e4f5872aa1b38520150de13f0
Luis Hanco <86266962+LuisHanco@users.noreply.github.com>
2024-05-26 18:44:08 -0500

    @LuisHanco

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// turn pages whenm click next or prev button
+const pageTurnBtn=document.querySelectorAll('.nextprec-btn');
+
+pageTurnBtn.forEach((el, index) =>{
+
+    el.onclick= () =>{
+        const pageTurnId = el.getAttribute('data-page');
+        const pageTurn =document.getElementById(pageTurnId);
+
+        if(pageTurn.classList.contains('turn')){
+            pageTurn.classList.remove('turn');
+            setTimeout(() =>{
+                pageTurn.style.zIndex = 20 - index;
+            },500);
+        }else{
+            pageTurn.classList.add('turn');
+            setTimeout(() =>{
+                pageTurn.style.zIndex = 20 - index;
+            },500);
+        }
+    }
+})
