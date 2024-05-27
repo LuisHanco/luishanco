cb0fc723183e4cb659765229ba252eeedb28d8e5
Luis Hanco <86266962+LuisHanco@users.noreply.github.com>
2024-05-26 23:52:40 -0500

    @LuisHanco

--- js/script.js
+++ js/script.js
@@ -11,12 +11,100 @@ pageTurnBtn.forEach((el, index) =>{
             pageTurn.classList.remove('turn');
             setTimeout(() =>{
                 pageTurn.style.zIndex = 20 - index;
-            },500);
+            },500)
         }else{
             pageTurn.classList.add('turn');
             setTimeout(() =>{
-                pageTurn.style.zIndex = 20 - index;
+                pageTurn.style.zIndex = 20 + index;
             },500);
         }
     }
 })
+
+
+// contact me button whe click
+
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me');
+
+contactMeBtn.onclick =() =>{
+    pages.forEach((page, index) =>{
+        setTimeout(() => {
+            page.classList.add('turn');
+
+            setTimeout(() => {
+                page.style.zIndex = 20 + index;
+            }, 500)
+        }, (index+1)*200+100)
+    })
+}
+
+// //create reserve index funtion
+
+let totalPages =pages.length;
+let pageNumber=0;
+
+
+function reverseIndex(){
+    pageNumber--;
+    if(pageNumber < 0){
+        pageNumber = totalPages - 1;
+    }
+}
+
+
+// back profile button when click
+
+
+
+
+const backProfileBtn = document.querySelector('.back-profile');
+
+backProfileBtn.onclick = () =>{
+    pages.forEach((_, index) =>{
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+
+            setTimeout(() => {
+                reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
+        }, (index + 1 ) * 200 + 100)
+    })
+}
+
+//opening animaition 
+
+const coverRight = document.querySelector('.cover.cover-right');
+const pageLeft = document.querySelector('.book-page .page-left');
+
+
+//openning animation cover right animation
+
+setTimeout(() => {
+    coverRight.classList.add('turn');
+}, 2100)
+
+setTimeout(() => {
+    coverRight.style.zIndex = -1;
+}, 2800)
+
+//openning animation cover left animation profile
+
+setTimeout(() => {
+    pageLeft.style.zIndex = 20;
+}, 3200)
+
+
+    pages.forEach((_, index) =>{
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+
+            setTimeout(() => {
+                reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
+        }, (index + 1 ) * 200 + 2100)
+    })
